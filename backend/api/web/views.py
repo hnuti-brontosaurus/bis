f2e10e01df7e3e4dcf75b607f43aec971a5f54a0
Ondřej Lomič <ondrej.lomic@gmail.com>
2023-03-06 11:50:35 +0100

    fix: old url addresses

--- backend/api/web/views.py
+++ backend/api/web/views.py
@@ -1,4 +1,6 @@
+from django.http import Http404
 from django.utils.timezone import now
+from rest_framework.generics import get_object_or_404
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from administration_units.models import AdministrationUnit
@@ -11,6 +13,28 @@ from opportunities.models import Opportunity
 class EventViewSet(ReadOnlyModelViewSet):
     serializer_class = EventSerializer
     filterset_class = EventFilter
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Perform the lookup filtering.
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+                'Expected view %s to be called with a URL keyword argument '
+                'named "%s". Fix your URL conf, or set the `.lookup_field` '
+                'attribute on the view correctly.' %
+                (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        try:
+            obj = get_object_or_404(queryset, **filter_kwargs)
+        except Http404:
+            obj = get_object_or_404(queryset, **{"_import_id": self.kwargs[lookup_url_kwarg]})
+
+        self.check_object_permissions(self.request, obj)
+        return obj
 
     def get_queryset(self):
         queryset = Event.objects.exclude(
