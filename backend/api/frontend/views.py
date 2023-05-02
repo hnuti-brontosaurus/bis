d0e7b36ad1e1902ebdae15d9dcc2e9c7e672e94d
Ondřej Lomič <ondrej.lomic@gmail.com>
2023-05-02 12:57:42 +0200

    fix: export #deploy

--- backend/api/frontend/views.py
+++ backend/api/frontend/views.py
@@ -302,4 +302,4 @@ def get_participants_list(request, event_id):
     if not Permissions(request.user, Event, 'frontend').has_change_permission(event):
         return HttpResponseForbidden()
 
-    return export.export_to_xlsx(..., ..., Event.objects.filter(id=event.id))
+    return export.export_to_xlsx(..., ..., event.record.participants.all())
