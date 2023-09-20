e144f33f26bb7fd81337e120e3a80e3e743ae328
Ondřej Lomič <ondrej.lomic@gmail.com>
2023-09-20 11:32:27 +0200

    fix: retries for ecomail

--- backend/ecomail/helpers.py
+++ backend/ecomail/helpers.py
@@ -2,8 +2,10 @@ from typing import Union, List
 
 import requests
 from django.conf import settings
+from requests.adapters import HTTPAdapter
 from rest_framework.utils import json
 from simplejson import JSONDecodeError
+from urllib3.util import Retry
 
 from ecomail.models import Contact, ContactLog
 
@@ -18,10 +20,15 @@ def send(contacts: Union[Contact, List[Contact]], method, uri, data=None, params
     if not isinstance(contacts, list):
         contacts = [contacts]
 
-    response = requests.request(method, BASE_URI + uri,
-                                json=data,
-                                params=params,
-                                headers={'key': settings.ECOMAIL_API_KEY})
+    s = requests.Session()
+    retries = Retry(total=4,
+                    backoff_factor=0.1,
+                    allowed_methods=False)
+    s.mount(BASE_URI, HTTPAdapter(max_retries=retries))
+    response = s.request(method, BASE_URI + uri,
+                         json=data,
+                         params=params,
+                         headers={'key': settings.ECOMAIL_API_KEY})
 
     log = f"uri: {uri}\n\n" \
           f"status_code: {response.status_code}\n\n" \
