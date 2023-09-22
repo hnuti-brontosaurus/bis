730d3e10ca8702b6fdeb51b8cc84dd1f191f1902
Martina Zelenkova <martina.zelenkova@logex.com>
2023-09-22 17:25:41 +0200

    previously removed code put back

--- frontend/src/org/components/EventForm/steps/registration/ShowApplicationModal.tsx
+++ frontend/src/org/components/EventForm/steps/registration/ShowApplicationModal.tsx
@@ -8,7 +8,7 @@ import type {
 } from 'app/services/bisTypes'
 import { EmailButton, PhoneButton, StyledModal, DataView } from 'components'
 import styles from '../ParticipantsStep.module.scss'
-import { FC } from 'react'
+import { FC, Fragment } from 'react'
 import { mergeWith, omit } from 'lodash'
 import { withOverwriteArray } from 'utils/helpers'
 import * as combinedTranslations from 'config/static/combinedTranslations'
@@ -33,7 +33,12 @@ export const ShowApplicationModal: FC<IShowApplicationModalProps> = ({
   currentApplication: currentApplicationProp,
   eventName,
   eventId,
+  // setCurrentApplicationId,
+  // setShowAddParticipantModal,
+  // deleteEventApplication,
   userId,
+  categories,
+  administrationUnits,
   currentParticipant,
   participantsMap,
 }) => {
@@ -59,6 +64,16 @@ export const ShowApplicationModal: FC<IShowApplicationModalProps> = ({
         }
       : skipToken,
   )
+  // we'll also show last year membership till end of February
+  // we want to give people time to register for the new year
+  // and still show continuity of membership
+  const currentYear = new Date().getFullYear()
+  const currentMonth = new Date().getMonth()
+  const currentMemberships = (user?.memberships ?? []).filter(
+    membership =>
+      membership.year === currentYear ||
+      (currentMonth < 2 && membership.year === currentYear - 1),
+  )
   // TODO consider showing historical memberships, too
 
   const formattedUser = mergeWith(
@@ -75,90 +90,189 @@ export const ShowApplicationModal: FC<IShowApplicationModalProps> = ({
       onClose={onClose}
       title={`Přihláška na akci ${eventName}`}
     >
-      {currentApplication && (
+      {currentApplication &&
+        (!participantsMap ||
+          !user ||
+          (user && currentApplication.id.toString() === user.id)) && (
+          <div>
+            {' '}
+            <h3>Přihláška:</h3>
+            <div className={styles.showUserApplicationNameBox}>
+              <h4>
+                {currentApplication.first_name} {currentApplication.last_name}{' '}
+                {currentApplication.nickname &&
+                  `(${currentApplication.nickname})`}{' '}
+              </h4>
+            </div>
+            {currentApplication.birthday && (
+              <div>
+                <span>Datum narození: </span>
+                <span>{currentApplication.birthday}</span>
+              </div>
+            )}
+            {currentApplication.pronoun?.name && (
+              <div>
+                <span>Oslovení: </span>
+                <span>{currentApplication.pronoun.name}</span>
+              </div>
+            )}
+            {currentApplication.email && (
+              <div>
+                <span>E-mail: </span>
+                <span>
+                  <EmailButton>{currentApplication.email}</EmailButton>
+                </span>
+              </div>
+            )}
+            {currentApplication.phone && (
+              <div>
+                <span>Telefon: </span>
+                <span>
+                  <PhoneButton>{currentApplication.phone}</PhoneButton>
+                </span>
+              </div>
+            )}
+            {currentApplication.health_issues && (
+              <div>
+                <span>Zdravotní omezení: </span>
+                <span>{currentApplication.health_issues}</span>
+              </div>
+            )}
+            {currentApplication.close_person && (
+              <div>
+                <span>Blízká osoba: </span>
+                <span>{`${currentApplication.close_person.first_name} ${currentApplication.close_person.last_name}`}</span>
+                {currentApplication.close_person.email && (
+                  <>
+                    <span>email: nnnn</span>
+                    <EmailButton>
+                      {currentApplication.close_person.email}
+                    </EmailButton>
+                  </>
+                )}
+                {currentApplication.close_person.phone && (
+                  <>
+                    <span>tel: </span>
+                    <PhoneButton>
+                      {currentApplication.close_person.phone}
+                    </PhoneButton>
+                  </>
+                )}
+              </div>
+            )}
+            {currentApplication.answers &&
+              currentApplication.answers.map(answer => (
+                <div key={answer.question.id}>
+                  <div>
+                    <h4>{answer.question.question}</h4>
+                  </div>
+                  <div>{answer.answer}</div>
+                </div>
+              ))}
+          </div>
+        )}
+      {userId && user && (
         <div>
-          {' '}
-          <h3>Přihláška:</h3>
-          <div className={styles.showUserApplicationNameBox}>
+          <div className={styles.addedUserBlock}>
+            <h3>Uživatel přidaný na akci: </h3>
             <h4>
-              {currentApplication.first_name} {currentApplication.last_name}{' '}
-              {currentApplication.nickname &&
-                `(${currentApplication.nickname})`}{' '}
+              {user.first_name} {user.last_name}{' '}
+              {user.nickname && `(${user.nickname})`}{' '}
             </h4>
           </div>
-          {currentApplication.birthday && (
+
+          {user.birthday && (
             <div>
               <span>Datum narození: </span>
-              <span>{currentApplication.birthday}</span>
+              <span>{user.birthday}</span>
             </div>
           )}
-          {currentApplication.pronoun?.name && (
+          {user.pronoun?.name && (
             <div>
               <span>Oslovení: </span>
-              <span>{currentApplication.pronoun.name}</span>
+              <span>{user.pronoun.name}</span>
             </div>
           )}
-          {currentApplication.email && (
+
+          {user.email && (
             <div>
               <span>E-mail: </span>
               <span>
-                <EmailButton>{currentApplication.email}</EmailButton>
+                <EmailButton>{user.email}</EmailButton>
               </span>
             </div>
           )}
-          {currentApplication.phone && (
+          {user.phone && (
             <div>
               <span>Telefon: </span>
               <span>
-                <PhoneButton>{currentApplication.phone}</PhoneButton>
+                <PhoneButton>{user.phone}</PhoneButton>
               </span>
             </div>
           )}
-          {currentApplication.health_issues && (
+          {user.health_issues && (
             <div>
               <span>Zdravotní omezení: </span>
-              <span>{currentApplication.health_issues}</span>
+              <span>{user.health_issues}</span>
             </div>
           )}
-          {currentApplication.close_person && (
+          {user.close_person && (
             <div>
               <span>Blízká osoba: </span>
-              <span>{`${currentApplication.close_person.first_name} ${currentApplication.close_person.last_name}`}</span>
-              {currentApplication.close_person.email && (
-                <>
-                  <span>email: nnnn</span>
-                  <EmailButton>
-                    {currentApplication.close_person.email}
-                  </EmailButton>
-                </>
+              <span>{`${user.close_person.first_name} ${user.close_person.last_name}`}</span>
+              {user.close_person.email && (
+                <span>
+                  'email: ' <EmailButton>{user.close_person.email}</EmailButton>
+                </span>
               )}
-              {currentApplication.close_person.phone && (
-                <>
-                  <span>tel: </span>
-                  <PhoneButton>
-                    {currentApplication.close_person.phone}
-                  </PhoneButton>
-                </>
+              {user.close_person.phone && (
+                <span>
+                  'tel: ' <PhoneButton>{user.close_person.phone}</PhoneButton>
+                </span>
               )}
             </div>
           )}
-          {currentApplication.answers &&
-            currentApplication.answers.map(answer => (
-              <div key={answer.question.id}>
-                <div>
-                  <h4>{answer.question.question}</h4>
-                </div>
-                <div>{answer.answer}</div>
+          {currentMemberships && currentMemberships.length !== 0 && (
+            <div>
+              <span>Členství: </span>
+              <div>
+                {currentMemberships.map(membership => {
+                  return (
+                    <Fragment
+                      key={`${membership.year}-${membership.administration_unit}-${membership.category}`}
+                    >
+                      {administrationUnits && categories && (
+                        <div>
+                          {
+                            administrationUnits.find(
+                              unit =>
+                                membership.administration_unit === unit.id,
+                            )?.name
+                          }
+                          {' - '}
+                          {
+                            categories.find(
+                              category =>
+                                membership.category.id === category.id,
+                            )?.name
+                          }{' '}
+                          ({membership.year})
+                        </div>
+                      )}
+                    </Fragment>
+                  )
+                })}
               </div>
-            ))}
+            </div>
+          )}
         </div>
       )}
-
-      {user &&
+      {userId &&
+        user &&
         currentApplication &&
         currentApplication.email === formattedUser.email && (
           <div>
-            <h3>Uživatel:</h3>
+            <h3>Detaily uživatele:</h3>
             <DataView
               data={formattedUser}
               translations={combinedTranslations.user}
