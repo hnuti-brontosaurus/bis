ce11f2de8a4ba800a574ef82ad0657bb5fee89a1
Ondřej Lomič <ondrej.lomic@gmail.com>
2024-01-20 09:49:29 +0100

    fix: back records upload

--- backend/donations/helpers.py
+++ backend/donations/helpers.py
@@ -9,45 +9,33 @@ from donations.models import Donation, Donor
 def upload_bank_records(file):
     assert file.name.endswith(".csv"), "Soubor není ve formátu .csv"
 
-    column_names = [
+    required_columns = [
         "Datum",
         "Objem",
-        "Měna",
-        "Číslo účtu",
-        "Protiúčet",
-        "Kód banky",
-        "KS",
-        "VS",
-        "SS",
-        "Poznámka",
-        "Zpráva pro příjemce",
-        "Typ",
-        "Upřesnění - objem",
-        "Upřesnění - měna",
         "VS",
     ]
 
     data = StringIO(file.read().decode("utf-8").strip())
-    data = list(csv.reader(data, delimiter=";"))
+    data = list(csv.reader(data, delimiter=","))
     header, data = data[0], data[1:]
 
-    for i, column in enumerate(column_names):
-        assert column in header[i], f"{i+1}. sloupec není {column}"
+    header = {i: column.strip() for i, column in enumerate(header)}
+    for column in required_columns:
+        assert column in header.values(), f"Sloupec {column} není přítomen"
 
     source = DonationSourceCategory.objects.get(slug="bank_transfer")
     for row in data:
-        day, month, year = row[0].split(".")
-        variable_symbol = row[7] or None
+        row = {header[i]: row[i] for i in range(len(row))}
+        day, month, year = row["Datum"].split(".")
+        variable_symbol = row["VS"] or None
         donor = Donor.objects.filter(
             variable_symbols__variable_symbol=variable_symbol
         ).first()
         Donation.objects.get_or_create(
             donor=donor,
             donated_at=date(int(year), int(month), int(day)),
-            amount=round(float(row[1].replace(",", "."))),
+            amount=round(float(row["Objem"].replace(",", "."))),
             donation_source=source,
             _variable_symbol=variable_symbol,
-            info="\n".join(
-                [f"{column_names[i]}: {row[i]}" for i in range(len(column_names))]
-            ),
+            info="\n".join([f"{key}: {value}" for key, value in row.items()]),
         )
