38864a8630aeec621084c163c145d59415a70c05
dag-mara <sucha.dagmar@gmail.com>
2024-06-08 14:50:58 +0200

    validation message for url input edited

--- frontend/src/org/components/EventForm/steps/PropagationStep.tsx
+++ frontend/src/org/components/EventForm/steps/PropagationStep.tsx
@@ -17,6 +17,7 @@ import { ReactElement, useEffect } from 'react'
 import { Controller, FormProvider } from 'react-hook-form'
 import { required } from 'utils/validationMessages'
 import { MethodsShapes } from '..'
+import * as validationMessages from 'utils/validationMessages'
 
 const foodIcons: { [name: string]: ReactElement } = {
   's masem': <Piglet />,
@@ -240,14 +241,19 @@ export const PropagationStep = ({
           </FormSection>
           <FormSection
             header="Web o akci"
-            help="Možnost přidat odkaz na webovou stránku vaší akce."
+            help='Možnost přidat odkaz na webovou stránku vaší akce. Odkaz vkládejte včetně "https://"'
             onWeb
           >
             <FormInputError>
               <input
                 type="url"
                 id="propagation.web_url"
-                {...register('propagation.web_url', {})}
+                {...register('propagation.web_url', {
+                  pattern: {
+                    value: /^(https?):\/\/[^\s/$.?#].[^\s]*$/,
+                    message: validationMessages.url,
+                  },
+                })}
               />
             </FormInputError>
           </FormSection>
