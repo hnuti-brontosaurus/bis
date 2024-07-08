bb581477c7bc3699842b6dc89947f9b5f686d643
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2024-07-08 14:49:56 +0200

    feat: feedback personal info

--- frontend/src/pages/EventFeedback/EventFeedbackForm.tsx
+++ frontend/src/pages/EventFeedback/EventFeedbackForm.tsx
@@ -1,6 +1,43 @@
 import { FC } from 'react'
 import { WebFeedbackForm } from 'app/services/bis'
+import { FormProvider, useForm } from 'react-hook-form'
+import {
+  FormInputError,
+  FormSection,
+  FormSectionGroup,
+  InlineSection,
+  Label,
+} from 'components'
 
 export const EventFeedbackForm: FC<{ feedbackForm: WebFeedbackForm }> = ({
   feedbackForm,
-}) => <>{feedbackForm.introduction && <div>{feedbackForm.introduction}</div>}</>
+}) => {
+  const methods = useForm()
+  const { register } = methods
+  return (
+    <>
+      {feedbackForm.introduction && <div>{feedbackForm.introduction}</div>}
+      <FormProvider {...methods}>
+        <form>
+          <FormSectionGroup>
+            <FormSection header="Osobní údaje">
+              <InlineSection>
+                <Label>Jméno</Label>
+                <FormInputError>
+                  <input type="text" {...register('name')} />
+                </FormInputError>
+              </InlineSection>
+              <InlineSection>
+                <Label>E-mail</Label>
+                <FormInputError>
+                  <input type="email" {...register('email')} />
+                </FormInputError>
+              </InlineSection>
+            </FormSection>
+            <FormSection header="Dotazník">TODO</FormSection>
+          </FormSectionGroup>
+        </form>
+      </FormProvider>
+    </>
+  )
+}
