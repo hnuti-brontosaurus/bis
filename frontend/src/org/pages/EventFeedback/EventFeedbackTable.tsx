cb79469b270b9245ae91ac3e3d5c584daf41d6dc
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2024-08-07 10:55:29 +0200

    feat: feedback table overflow

--- frontend/src/org/pages/EventFeedback/EventFeedbackTable.tsx
+++ frontend/src/org/pages/EventFeedback/EventFeedbackTable.tsx
@@ -8,28 +8,33 @@ export const EventFeedbackTable: FC<{
   feedbacks: EventFeedbackRead[]
   onRowClick: (id: number) => void
 }> = ({ inquiries, feedbacks, onRowClick }) => (
-  <table className={styles.table}>
-    <thead>
-      <tr>
-        <th>Jméno</th>
-        <th>E-mail</th>
-        {inquiries.map(({ inquiry, id }) => (
-          <th key={id}>{inquiry}</th>
-        ))}
-      </tr>
-    </thead>
-    <tbody>
-      {feedbacks.map(feedback => (
-        <tr key={feedback.id}>
-          <td onClick={() => onRowClick(feedback.id)}>{feedback.name}</td>
-          <td onClick={() => onRowClick(feedback.id)}>{feedback.email}</td>
-          {inquiries.map(({ id }) => (
-            <td key={id} onClick={() => onRowClick(feedback.id)}>
-              {feedback.replies.find(({ inquiry }) => inquiry.id === id)?.reply}
-            </td>
+  <div className={styles.container}>
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          <th>Jméno</th>
+          <th>E-mail</th>
+          {inquiries.map(({ inquiry, id }) => (
+            <th key={id}>{inquiry}</th>
           ))}
         </tr>
-      ))}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        {feedbacks.map(feedback => (
+          <tr key={feedback.id}>
+            <td onClick={() => onRowClick(feedback.id)}>{feedback.name}</td>
+            <td onClick={() => onRowClick(feedback.id)}>{feedback.email}</td>
+            {inquiries.map(({ id }) => (
+              <td key={id} onClick={() => onRowClick(feedback.id)}>
+                {
+                  feedback.replies.find(({ inquiry }) => inquiry.id === id)
+                    ?.reply
+                }
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
 )
