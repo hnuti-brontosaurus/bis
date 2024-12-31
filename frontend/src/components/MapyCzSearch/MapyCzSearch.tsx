ed8aabfd363718930610b4d229888c6770ac0f21
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2024-12-31 15:24:39 +0100

    feature: do not retain search value when selected

--- frontend/src/components/MapyCzSearch/MapyCzSearch.tsx
+++ frontend/src/components/MapyCzSearch/MapyCzSearch.tsx
@@ -1,6 +1,6 @@
 import { useDebouncedState } from 'hooks/debouncedState'
 import { MapItem, useMapSuggest } from 'hooks/useMapSuggest'
-import { FC, useState } from 'react'
+import { FC } from 'react'
 import Select, { components, MenuProps } from 'react-select'
 
 import style from './MapyCzSearch.module.scss'
@@ -32,12 +32,10 @@ export const MapyCzSearch = ({
   // unused prop to ensure that the component has the same interface as OSMSearch
   onError: (error: Error) => void
 }) => {
-  const [value, setValueInternal] = useState<MapItem | null>(null)
   const [query, debouncedQuery, setQuery] = useDebouncedState(1000, '')
   const options = useMapSuggest(debouncedQuery)
 
   const setValue = (newValue: MapItem | null) => {
-    setValueInternal(newValue)
     if (newValue) {
       onSelect([newValue.position.lat, newValue.position.lon], newValue.name)
     }
@@ -48,7 +46,7 @@ export const MapyCzSearch = ({
       options={options}
       inputValue={query}
       onInputChange={setQuery}
-      value={value}
+      value={null}
       onChange={setValue}
       getOptionValue={({ name }) => name}
       formatOptionLabel={OptionLabel}
