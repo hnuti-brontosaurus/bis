dcabae8a85af7f5a4615bb666163b53f6c624c59
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2025-01-16 16:05:33 +0100

    feature: behaviour issues styling and explanation

--- frontend/src/org/components/EventForm/steps/registration/BehaviourIssuesTooltip.tsx
+++ frontend/src/org/components/EventForm/steps/registration/BehaviourIssuesTooltip.tsx
@@ -3,14 +3,20 @@ import { FC } from 'react'
 import { IoWarning } from 'react-icons/io5'
 import Tooltip from 'react-tooltip-lite'
 
+import style from './BehaviourIssuesTooltip.module.scss'
+
 interface Props {
   user?: User
 }
 
 export const BehaviourIssuesTooltip: FC<Props> = ({ user }) =>
   user?.behaviour_issues ? (
-    <Tooltip useDefaultStyles content={user.behaviour_issues} tagName="span">
-      {' '}
+    <Tooltip
+      useDefaultStyles
+      content={user.behaviour_issues}
+      tagName="span"
+      className={style.main}
+    >
       <IoWarning />
     </Tooltip>
   ) : null
