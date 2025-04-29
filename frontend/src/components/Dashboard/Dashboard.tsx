899c4f0c58fbaf9773ebcb76413f2e07136bfb46
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2025-04-29 14:44:16 +0200

    feature: very basic dashboard items styling

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,16 +1,19 @@
 import { FC } from 'react'
 import { DashboardItem } from 'app/services/testApi'
 import { formatDateTime } from 'utils/helpers'
+import styles from './Dashboard.module.scss'
 
 export const Dashboard: FC<{ items: DashboardItem[] }> = ({ items }) => (
-  <table>
-    <tbody>{items.map(DashboardRow)}</tbody>
-  </table>
+  <div className={styles.main}>
+    <table>
+      <tbody>{items.map(DashboardRow)}</tbody>
+    </table>
+  </div>
 )
 
 const DashboardRow: FC<DashboardItem> = ({ date, name, description }) => (
   <>
-    <tr>
+    <tr className={styles.row}>
       <td>{formatDateTime(date)}</td>
       <td>{name}</td>
     </tr>
