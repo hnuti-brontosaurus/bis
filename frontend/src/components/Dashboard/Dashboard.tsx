40b18a3332e5c3d8456dcdb6ea499f3b9476a971
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2025-04-29 14:58:47 +0200

    feature: dashboard styling w/ description

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import { DashboardItem } from 'app/services/testApi'
 import { formatDateTime } from 'utils/helpers'
+import classNames from 'classnames'
 import styles from './Dashboard.module.scss'
 
 export const Dashboard: FC<{ items: DashboardItem[] }> = ({ items }) => (
@@ -13,12 +14,12 @@ export const Dashboard: FC<{ items: DashboardItem[] }> = ({ items }) => (
 
 const DashboardRow: FC<DashboardItem> = ({ date, name, description }) => (
   <>
-    <tr className={styles.row}>
+    <tr className={classNames(styles.title, { [styles.single]: !description })}>
       <td>{formatDateTime(date)}</td>
       <td>{name}</td>
     </tr>
     {description && (
-      <tr>
+      <tr className={styles.description}>
         <td colSpan={2}>{description}</td>
       </tr>
     )}
