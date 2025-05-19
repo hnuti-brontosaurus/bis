303a15d1301bff5d817a3a3b07febc1dcb62a101
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2025-05-19 11:29:20 +0200

    feature: move dashboard formatting to backend

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react'
 import { DashboardItem } from 'app/services/testApi'
-import { formatDateTime } from 'utils/helpers'
 import classNames from 'classnames'
 import styles from './Dashboard.module.scss'
 
@@ -10,14 +9,19 @@ export const Dashboard: FC<{ items: DashboardItem[] }> = ({ items }) => (
 
 const DashboardRow: FC<DashboardItem> = ({ date, name, description }) => (
   <>
-    <div className={classNames(styles.date, { [styles.single]: !description })}>
-      {formatDateTime(date)}
-    </div>
+    <div
+      className={classNames(styles.date, { [styles.single]: !description })}
+      dangerouslySetInnerHTML={{ __html: date }}
+    />
     <div
       className={classNames(styles.title, { [styles.single]: !description })}
-    >
-      {name}
-    </div>
-    {description && <div className={styles.description}>{description}</div>}
+      dangerouslySetInnerHTML={{ __html: name }}
+    />
+    {description && (
+      <div
+        className={styles.description}
+        dangerouslySetInnerHTML={{ __html: description }}
+      />
+    )}
   </>
 )
