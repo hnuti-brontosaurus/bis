753ce95640a9de5a92b2e3cdc910015aa5e08cdf
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2025-12-08 12:05:06 +0100

    feature: change event group to event duration

--- frontend/src/config/static/translations.ts
+++ frontend/src/config/static/translations.ts
@@ -449,8 +449,8 @@ export const membershipCategory = {
 } as const
 
 export const eventGroupCategory = {
-  _name: 'Druh akce',
-  _name_plural: 'Druh akce',
+  _name: 'Délka akce',
+  _name_plural: 'Délka akce',
 } as const
 
 export const eventCategory = {
