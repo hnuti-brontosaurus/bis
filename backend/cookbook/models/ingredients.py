ee3d1f6139544af04b44cbb73f6a84ce79c2b7a5
Ondřej Lomič <ondrej.lomic@gmail.com>
2026-01-06 19:16:02 +0100

    feat: cookbook edit recipes and auth

---
+++ backend/cookbook/models/ingredients.py
@@ -0,0 +1,17 @@
+from cookbook.models.base import BaseModel, ChangeMixin
+from django.db.models import *
+from translation.translate import translate_model
+
+
+@translate_model
+class Ingredient(ChangeMixin, BaseModel):
+    name = CharField(max_length=31, unique=True)
+    state = CharField(
+        max_length=13,
+        choices=[("solid", "Pevná"), ("liquid", "Tekuté")],
+        default="solid",
+    )
+    g_per_piece = PositiveSmallIntegerField(blank=True, null=True)
+    g_per_liter = PositiveSmallIntegerField(blank=True, null=True)
+    g_per_serving = PositiveSmallIntegerField(blank=True, null=True)
+    reasoning = TextField(blank=True)
