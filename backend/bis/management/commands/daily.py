842a045eac95103ac60072a01a820757d5a0c4cb
Jan Malý <malyjan3581@gmail.com>
2026-01-20 10:34:15 +0100

    create Pledge and DonorEvent model, substitute has_recurrent_donation atribute on Donor

--- backend/bis/management/commands/daily.py
+++ backend/bis/management/commands/daily.py
@@ -40,46 +40,47 @@ def check_disk_space():
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        try_to_run(call_command, "create_categories")
-        try_to_run(call_command, "update_dashboard_items")
-        try_to_run(call_command, "record_history")
-        try_to_run(call_command, "import_locations")
+        # try_to_run(emails.events_summary)
+        # try_to_run(call_command, "create_categories")
+        # try_to_run(call_command, "update_dashboard_items")
+        # try_to_run(call_command, "record_history")
+        # try_to_run(call_command, "import_locations")
         try_to_run(call_command, "import_donations")
-        try_to_run(call_command, "set_unique_user_str")
-        try_to_run(call_command, "set_date_joined")
-        try_to_run(emails.event_not_closed_10_days)
-        try_to_run(emails.event_not_closed_20_days)
-        try_to_run(emails.qualification_about_to_end)
-        try_to_run(emails.qualification_ended)
-        try_to_run(SavedFile.remove_old)
-
-        today = date.today()
-        # weekly
-        if today.weekday() == 0:
-            try_to_run(emails.events_summary)
-            try_to_run(emails.opportunities_created_summary)
-            try_to_run(check_disk_space)
-            try_to_run(emails.expressed_engagement_in_feedback)
-
-        # monthly
-        if today.day == 1:
-            try_to_run(call_command, "archive_events")
-
-        day_of_year = (today.day, today.month)
-
-        if day_of_year == (15, 10):
-            try_to_run(emails.fill_memberships, call=1)
-        if day_of_year == (27, 10):
-            try_to_run(emails.fill_memberships, call=2)
-
-        if day_of_year == (31, 1):
-            try_to_run(emails.qualification_ends_this_year)
-
-        if day_of_year in [
-            (15, 1),
-            (15, 3),
-            (15, 5),
-            (15, 9),
-            (30, 11),
-        ]:
-            try_to_run(emails.send_opportunities_summary)
+        # try_to_run(call_command, "set_unique_user_str")
+        # try_to_run(call_command, "set_date_joined")
+        # try_to_run(emails.event_not_closed_10_days)
+        # try_to_run(emails.event_not_closed_20_days)
+        # try_to_run(emails.qualification_about_to_end)
+        # try_to_run(emails.qualification_ended)
+        # try_to_run(SavedFile.remove_old)
+        #
+        # today = date.today()
+        # # weekly
+        # if today.weekday() == 0:
+        #     try_to_run(emails.events_summary)
+        #     try_to_run(emails.opportunities_created_summary)
+        #     try_to_run(check_disk_space)
+        #     try_to_run(emails.expressed_engagement_in_feedback)
+        #
+        # # monthly
+        # if today.day == 1:
+        #     try_to_run(call_command, "archive_events")
+        #
+        # day_of_year = (today.day, today.month)
+        #
+        # if day_of_year == (15, 10):
+        #     try_to_run(emails.fill_memberships, call=1)
+        # if day_of_year == (27, 10):
+        #     try_to_run(emails.fill_memberships, call=2)
+        #
+        # if day_of_year == (31, 1):
+        #     try_to_run(emails.qualification_ends_this_year)
+        #
+        # if day_of_year in [
+        #     (15, 1),
+        #     (15, 3),
+        #     (15, 5),
+        #     (15, 9),
+        #     (30, 11),
+        # ]:
+        #     try_to_run(emails.send_opportunities_summary)
