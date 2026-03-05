85fb01ca6836a5f275b1d643d9676682a95ac9d7
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2026-03-05 14:02:08 +0100

    chore: big feedback pruning

--- frontend/src/utils/getRequiredFeedbackInquiries.ts
+++ frontend/src/utils/getRequiredFeedbackInquiries.ts
@@ -1,25 +1,25 @@
 import { Optional } from 'utility-types'
-import { Event, InquiryRead } from 'app/services/bisTypes'
+import { InquiryRead } from 'app/services/bisTypes'
 
 type QuestionnaireSection = Optional<InquiryRead, 'id' | 'order'>[]
 
 const checkInSection: QuestionnaireSection = [
   {
-    inquiry: 'Anketka',
+    inquiry: 'Anketa',
     slug: 'check_in_section_heading',
     data: { type: 'header', fixed: true },
   },
   {
     inquiry:
-      'Pokolikáté jsi na akci s Hnutí Brontosaurus? (myslíme tábor nebo víkendovou akci)',
+      'Pokolikáté jsi na akci s Hnutím Brontosaurus? (myslíme tábor nebo víkendovou akci)',
     slug: 'previous_participation_number',
     is_required: true,
     data: {
       type: 'radio',
       options: [
         { option: 'poprvé' },
-        { option: 'podruhé nebo potřetí' },
-        { option: 'už jsem byl/a víckrát' },
+        { option: 'byl*a jsem už několikrát' },
+        { option: 'jezdím pravidelně' },
       ],
       layout: 'horizontal',
       fixed: true,
@@ -27,33 +27,36 @@ const checkInSection: QuestionnaireSection = [
   },
   {
     inquiry:
-      'Dozvěděl/a ses nejdříve o této akci nebo o Hnutí Brontosaurus jako celku?',
-    slug: 'previous_brontosaurus_knowledge',
+      'Věděl*a jsi, že akce je součástí Hnutí Brontosaurus a dostal*a jsi informace o tom, jaké akce Hnutí Brontosaurus pořádá?',
+    slug: 'brontosaurus_knowledge',
     is_required: true,
     data: {
       type: 'radio',
-      options: [{ option: 'O akci' }, { option: 'O Hnutí Brontosaurus' }],
+      options: [
+        { option: 'ano' },
+        { option: 'ne' },
+        { option: 'jezdím pravidelně' },
+      ],
       layout: 'horizontal',
       fixed: true,
     },
   },
   {
-    inquiry: 'Jak ses dozvěděl/a o této akci?',
+    inquiry: 'Jak ses o této akci dozvěděl*a?',
     slug: 'propagation_source',
     is_required: true,
     data: {
       type: 'radio',
       options: [
         { option: 'web Hnutí Brontosaurus' },
-        { option: 'sociální  sítě – FB' },
-        { option: 'sociální sítě – IG' },
-        { option: 'kamarád, známý nebo rodič' },
-        { option: 'tištěná propagace – plakát nebo leták' },
-        { option: 'weby s nabídkami akcí (dobrovolnik.cz atd.)' },
-        { option: 'dobrovolnická centra (MUNI Pomáhá atd.)' },
-        { option: 'akce a festivaly – Ekostan, Dort, aj.' },
+        { option: 'sociální sítě – Facebook' },
+        { option: 'sociální sítě – Instagram' },
+        { option: 'kamarád*ka, známý*á nebo rodič' },
+        { option: 'tištěná propagace (plakát, leták)' },
+        { option: 'weby s nabídkou akcí (dobrovolnik.cz apod.)' },
+        { option: 'dobrovolnické centrum (MUNI Pomáhá apod.)' },
+        { option: 'akce nebo festival (Ekostan, Dort aj.)' },
         { option: 'Roverský kmen' },
-        { option: 'Google' },
         { option: 'nepamatuji si' },
       ],
       fixed: true,
@@ -62,19 +65,19 @@ const checkInSection: QuestionnaireSection = [
   {
     // TODO this one should have optional comment
     inquiry:
-      'Podle čeho sis vybral/a tuto akci? Prosím vyber pro tebe nejdůležitější 3 důvody',
+      'Proč sis vybral*a právě tuto akci? Prosím vyber tři nejdůležitější důvody a budeme rádi i za krátký komentář.',
     slug: 'participation_reasons',
     is_required: true,
     data: {
       type: 'checkbox',
       options: [
-        { option: 'Termín' },
-        { option: 'Místo' },
-        { option: 'Práce' },
-        { option: 'Téma' },
-        { option: 'Kamarádi, účastníci' },
-        { option: 'Organizátoři' },
-        { option: 'Pořádající základní článek/klub' },
+        { option: 'termín' },
+        { option: 'místo' },
+        { option: 'dobrovolnická pomoc' },
+        { option: 'téma' },
+        { option: 'kamarádi/účastníci' },
+        { option: 'organizátoři' },
+        { option: 'pořádající základní článek/klub' },
       ],
       fixed: true,
     },
@@ -82,7 +85,7 @@ const checkInSection: QuestionnaireSection = [
   {
     // TODO remove when comment can be added to previous inquiry
     inquiry:
-      'Chceš nám napsat něco více k tomu, proč sis vybral/a tuto akci? Budeme rádi za jakýkoli komentář',
+      'Chceš nám napsat něco více k tomu, proč sis vybral*a tuto akci? Budeme rádi za jakýkoli komentář',
     slug: 'participation_reasons_comment',
     is_required: false,
     data: {
@@ -100,21 +103,14 @@ const eventSection: QuestionnaireSection = [
   },
 
   {
-    inquiry: 'Komunikace před akcí byla jasná a měl/a jsem dostatek informací ',
+    inquiry: 'Komunikace před akcí byla jasná a měl*a jsem dostatek informací ',
     slug: 'pre_event_communication',
     is_required: true,
     data: { type: 'scale', comment: true, fixed: true },
   },
   {
     inquiry:
-      'Věděl/a jsem, že akce je součástí neziskové organizace Hnutí Brontosaurus a byly mi poskytnuty informace o tom, jaké akce Hnutí Brontosaurus pořádá.',
-    slug: 'brontosaurus_communication',
-    is_required: true,
-    data: { type: 'scale', comment: true, fixed: true },
-  },
-  {
-    inquiry:
-      'Na akci jsem se cítil/a bezpečně a měl/a jsem se na koho v případě potřeby obrátit.',
+      'Na akci jsem se cítil*a bezpečně a měl*a jsem se na koho v případě potřeby obrátit.',
     slug: 'event_safety',
     is_required: true,
     data: { type: 'scale', comment: true, fixed: true },
@@ -127,16 +123,17 @@ const eventSection: QuestionnaireSection = [
     data: { type: 'scale', comment: true, fixed: true },
   },
   {
-    inquiry: 'Náplň akce mi přišla pestrá, zajímavá a dobře připravená',
-    slug: 'event_program',
+    inquiry: 'Akce mi rozšířila obzory v oblasti péče o přírodu nebo památky.',
+    slug: 'impact_self_volunteering',
     is_required: true,
     data: { type: 'scale', comment: true, fixed: true },
   },
   {
-    inquiry: 'Přístup organizátorů k účastníkům byl milý, nápomocný a upřímný.',
-    slug: 'organizer_attitude',
+    inquiry:
+      'Jaká byla celková atmosféra na akci? Co konkrétně se ti líbilo a co bys potřeboval*a jinak? Tvoje zpětná vazba nám umožní se stále zlepšovat.',
+    slug: 'event_atmosphere',
     is_required: true,
-    data: { type: 'scale', comment: true, fixed: true },
+    data: { type: 'text', fixed: true },
   },
 ]
 
@@ -146,63 +143,19 @@ const volunteeringSection: QuestionnaireSection = [
     slug: 'volunteering_section_heading',
     data: { type: 'header', fixed: true },
   },
-
   {
     inquiry:
-      'Měl/a jsem dostatek informací o účelu a smysluplnosti dobrovolnické činnosti.',
+      'Měl*a jsem dostatek informací o účelu a smysluplnosti dobrovolnické činnosti na pomoc přírodě, památkám nebo lidem.',
     slug: 'volunteering_communication',
     is_required: true,
     data: { type: 'scale', comment: true, fixed: true },
   },
   {
     inquiry:
-      'Já osobně jsem považoval/a dobrovolnickou činnost za smysluplnou a cítil/a jsem, že pomáhám přírodě, památkám nebo lidem.',
-    slug: 'volunteering_meaningfulness',
+      'Jak hodnotíš smysluplnost, náročnost a organizaci dobrovolnické pomoci?',
+    slug: 'volunteering_feedback',
     is_required: true,
-    data: { type: 'scale', comment: true, fixed: true },
-  },
-  {
-    inquiry:
-      'Dobrovolnická činnost byla přiměřeně náročná s dostatkem času na odpočinek.',
-    slug: 'volunteering_difficulty',
-    is_required: true,
-    data: { type: 'scale', comment: true, fixed: true },
-  },
-  {
-    inquiry:
-      'Dobrovolnickou činnost jsem si užil/a a bavila mě - pomůže nám, když nám napíšeš, co konkrétně tě na práci nejvíce bavilo a proč ti přišla užitečná.',
-    slug: 'volunteering_fun',
-    is_required: true,
-    data: { type: 'scale', comment: true, fixed: true },
-  },
-]
-
-const atmosphereSection: QuestionnaireSection = [
-  {
-    inquiry: 'Atmosféra',
-    slug: 'atmosphere_section_heading',
-    data: { type: 'header', fixed: true },
-  },
-  {
-    inquiry:
-      'Díky účasti na akci jsem si rozšířil/a obzory nebo získal/a nový pohled na to jak vnímám své okolí a okolní přírodu',
-    slug: 'impact_self',
-    is_required: true,
-    data: { type: 'scale', comment: true, fixed: true },
-  },
-  {
-    inquiry:
-      'Na akci jsem se seznámil/a s lidmi, s kterými bych rád/a dál udržovala kontakt i po akci',
-    slug: 'impact_people',
-    is_required: true,
-    data: { type: 'scale', comment: true, fixed: true },
-  },
-  {
-    inquiry:
-      'Jak hodnotíš celkovou atmosféru na akci? Co konkrétně se ti na akci líbilo a co bys rád/a vylepšil/a? Tvoje zpětná vazba nám umožní se stále zlepšovat.',
-    slug: 'event_atmosphere',
-    is_required: true,
-    data: { type: 'scale', comment: true, fixed: true },
+    data: { type: 'text', fixed: true },
   },
 ]
 
@@ -214,36 +167,30 @@ const involvementSection: QuestionnaireSection = [
   },
   {
     inquiry:
-      'Chceš se více zapojit do činnosti Hnutí Brontosaurus? Rádi ti s tím pomůžeme a přivítáme tě mezi sebou :) Co tě nejvíc láká? Možnosti jak se zapojit najdeš i na webu Zapoj se', // TODO link [Zapoj se](https://brontosaurus.cz/zapoj-se/)
+      'Chceš se do Hnutí Brontosaurus zapojit víc? Co tě nejvíc láká? Možnosti, jak se zapojit, najdeš v Aktuálních příležitostech.', // TODO link [Aktuálních příležitostech](https://brontosaurus.cz/zapoj-se/)
     slug: 'involvement_means',
     is_required: true,
     data: {
       type: 'checkbox',
       options: [
-        { option: 'přidat se do organizátorského týmu' },
-        { option: 'absolvovat organizátorský kurz' },
-        { option: 'zúčastnit se další akce Hnutí Brontosaurus' },
+        { option: 'přidat se do organizátorského týmu' }, // TODO link https://brontosaurus.cz/zapoj-se/
+        { option: 'absolvovat organizátorský kurz' }, // TODO link https://organizator.brontosaurus.cz/
+        { option: 'zúčastnit se další akce Hnutí Brontosaurus' }, // TODO link https://brontosaurus.cz/
         { option: 'vytvořit vlastní akci' },
         {
-          option: 'zapojit se do dobrovolnického centra v Brně nebo Praze',
-        },
-        { option: 'stát se vedoucím dětských Brďo oddílů' },
-        { option: 'stát se členem Hnutí Brontosaurus (Staň se členem)' }, // TODO link [Staň se členem](https://brontosaurus.cz/zapoj-se/clenstvi/)
-        {
           option:
-            'stát se podporovatelem Hnutí Brontosaurus (Adoptuj Brontosaura)', // TODO link [Adoptuj Brontosaura](https://brontosaurus.cz/podpor-nas/)
+            'zapojit se do dobrovolnického centra v Brně, Praze nebo Olomouci',
+        },
+        { option: 'stát se vedoucím Brontosauřích dětských oddílů' },
+        { option: 'stát se členem Hnutí Brontosaurus' }, // TODO link https://brontosaurus.cz/zapoj-se/clenstvi/
+        {
+          option: 'stát se podporovatelem Hnutí Brontosaurus', // TODO link https://brontosaurus.cz/podpor-nas/
         },
         { option: 'nechci se zapojit' },
+        { option: 'jsem zapojen*á až až' },
       ],
       fixed: true,
     },
-  },
-  {
-    inquiry:
-      'Poskytuje Hnutí Brontosaurus dostatek příležitostí, jak se zapojit? Jakým způsobem ti HB může pomoci, aby jsi udržel/a vzniklá přátelství a více se zapojil/a do činnosti HB (organizace táborů a další akce)',
-    slug: 'involvement_feedback',
-    is_required: true,
-    data: { type: 'scale', comment: true, fixed: true },
   },
 ]
 
@@ -253,6 +200,5 @@ export const getRequiredFeedbackInquiries = (
   checkInSection.concat(
     eventSection,
     isVolunteering ? volunteeringSection : [],
-    atmosphereSection,
     involvementSection,
   )
