5e2d0625e34e5fe81e72d30f1035809f5c1dfe46
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2026-03-13 10:55:52 +0100

    fix: only generate long classnames for development build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({
+export default defineConfig(({mode}) => ({
   plugins: [react(), tsconfigPaths(), svgr()],
   server: { host: '0.0.0.0', port: 3000, allowedHosts: ['frontend'] },
   build: { outDir: 'build' },
@@ -12,10 +12,10 @@ export default defineConfig({
     preprocessorOptions: {
       scss: { includePaths: ['src'] },
     },
-    modules: {
+    modules: mode === 'development' ? {
       generateScopedName: '[name]__[local]__[hash:base64:5]'
-    }
+    } : {}
   },
   // safety net for third-party libs that still reference process.env
   define: { 'process.env': {} },
-})
+}))
