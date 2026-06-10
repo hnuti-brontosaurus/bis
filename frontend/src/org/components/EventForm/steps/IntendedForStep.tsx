15e555ce74620aec5b2989265712d05f36a1e10d
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2026-06-10 14:29:59 +0200

    chore: remove VIP propagation

--- frontend/src/org/components/EventForm/steps/IntendedForStep.tsx
+++ frontend/src/org/components/EventForm/steps/IntendedForStep.tsx
@@ -3,59 +3,22 @@ import {
   FormInputError,
   FormSection,
   FormSectionGroup,
-  FormSubheader,
-  FormSubsection,
-  FullSizeElement,
-  InfoBox,
   InlineSection,
   Loading,
 } from 'components'
-import { useEffect } from 'react'
 import { Controller, FormProvider } from 'react-hook-form'
-import { getIdBySlug, requireBoolean } from 'utils/helpers'
-import { required, vipPropagationRequired } from 'utils/validationMessages'
+import { required } from 'utils/validationMessages'
 import { MethodsShapes } from '..'
 
 export const IntendedForStep = ({
   methods,
-  isCamp,
-  hasVIPPropagationOption,
 }: {
   methods: MethodsShapes['intendedFor']
-  isCamp: boolean
-  hasVIPPropagationOption: boolean
 }) => {
   const { data: intendedFor } =
     api.endpoints.readIntendedFor.useQuery(undefined)
   const { watch, register, trigger, control, unregister, setValue, formState } =
     methods
-
-  // unregister stuff
-  useEffect(() => {
-    const subscription = watch((data, { name }) => {
-      if (
-        name === 'intended_for' &&
-        getIdBySlug(
-          intendedFor?.results ?? [],
-          'for_first_time_participant',
-        ) !== data.intended_for
-      ) {
-        unregister('vip_propagation')
-        setValue('vip_propagation', null)
-      }
-    })
-
-    return () => subscription.unsubscribe()
-  }, [watch, intendedFor?.results, unregister, setValue])
-
-  // trigger vip_propagation validation
-  useEffect(() => {
-    if (formState.isSubmitted)
-      // trigger in next tick, to fix revalidating first field
-      Promise.resolve().then(() => trigger('vip_propagation'))
-  }, [trigger, hasVIPPropagationOption, formState.isSubmitted])
-
-  const prvoucastnici = 5
 
   if (!intendedFor) return <Loading>Připravujeme formulář</Loading>
 
@@ -100,168 +63,6 @@ export const IntendedForStep = ({
               />
             </FormInputError>
           </FormSection>
-          {
-            /* not great hardcoded id */
-            +watch('intended_for') === prvoucastnici &&
-              hasVIPPropagationOption && (
-                <>
-                  <FullSizeElement>
-                    <InfoBox>
-                      <p>
-                        Pokud chcete být zařazeni do VIP propagace, vyplňte
-                        všechny 3 otázky uvedené níže. VIP propagace je primárně
-                        určena pro tábory a víkendovky.
-                      </p>
-                      <p>
-                        Hnutí Brontosaurus pravidelně vytváří nabídku výběrových
-                        dobrovolnických akcí, kterými oslovujeme nové účastníky,
-                        zejména středoškolskou mládež a začínající vysokoškoláky
-                        (15 - 26 let). Cílem akce je oslovit tyto prvoúčastníky
-                        a mít jich nejlépe polovinu, (min. třetinu) z celkového
-                        počtu účastníků.
-                      </p>
-                      <p>Zadáním akce pro prvoúčastníky získáte:</p>
-                      <ul>
-                        <li>
-                          Širší propagaci skrze letáky, osobní kontakty apod.
-                          Zveřejnění na letáku VIP propagace.
-                        </li>
-                        <li>
-                          Propagaci na středních školách od lektorů
-                          středoškolských programů
-                        </li>
-                        <li>
-                          Zveřejnění na Facebooku a Instagramu HB, reklamu na
-                          Facebooku
-                        </li>
-                        <li>Reklamu v Google vyhledávání</li>
-                        <li>Služby grafika HB (dle dohodnutého rozsahu)</li>
-                        <li>Přidání do webových katalogů akcí</li>
-                        <li>Slevu na inzerci v Roverském kmenu (pro tábory)</li>
-                        <li>Zpětnou vazbu k webu a Facebooku akce</li>
-                        <li>Metodickou pomoc a pomoc s agendou akce</li>
-                        <li>Propagace na novém webu HB v sekci Jedu poprvé</li>
-                      </ul>
-                    </InfoBox>
-                  </FullSizeElement>
-
-                  <FullSizeElement>
-                    <FormSubheader required={hasVIPPropagationOption}>
-                      Cíle akce a přínos pro prvoúčastníky
-                    </FormSubheader>
-                    <InfoBox>
-                      Jaké je hlavní téma vaší akce? Jaké jsou hlavní cíle akce?
-                      Co nejvýstižněji popište, co akce přináší účastníkům, co
-                      zajímavého si zkusí, co se dozví, naučí, v čem se
-                      rozvinou…
-                    </InfoBox>
-                    <FormInputError>
-                      <textarea
-                        {...register('vip_propagation.goals_of_event', {
-                          required:
-                            hasVIPPropagationOption && vipPropagationRequired,
-                        })}
-                      ></textarea>
-                    </FormInputError>
-                  </FullSizeElement>
-
-                  <FullSizeElement>
-                    <FormSubheader required={hasVIPPropagationOption}>
-                      Programové pojetí akce pro prvoúčastníky
-                    </FormSubheader>
-                    <InfoBox>
-                      V základu uveďte, jak bude vaše akce programově a
-                      dramaturgicky koncipována (motivační příběh, zaměření
-                      programu &ndash; hry, diskuse, řemesla,...). Uveďte, jak
-                      náplň a program akce reflektují potřeby vaší cílové
-                      skupiny prvoúčastníků.
-                    </InfoBox>
-                    <FormInputError>
-                      <textarea
-                        {...register('vip_propagation.program', {
-                          required:
-                            hasVIPPropagationOption && vipPropagationRequired,
-                        })}
-                      ></textarea>
-                    </FormInputError>
-                  </FullSizeElement>
-
-                  <FullSizeElement>
-                    <FormSubheader required={hasVIPPropagationOption}>
-                      Krátký zvací text do propagace
-                    </FormSubheader>
-                    <InfoBox>
-                      Ve 2-4 větách nalákejte na vaši akci a zdůrazněte osobní
-                      přínos pro účastníky (max. 200 znaků).
-                    </InfoBox>
-                    <FormInputError>
-                      <textarea
-                        {...register('vip_propagation.short_invitation_text', {
-                          maxLength: 200,
-                          required:
-                            hasVIPPropagationOption && vipPropagationRequired,
-                        })}
-                      ></textarea>
-                    </FormInputError>
-                  </FullSizeElement>
-                  {/*
-                  // only "camp" can see this
-                  // https://docs.google.com/document/d/1p3nz0-kVJxwN_pRCYYyhy0BObyGox6LDk35RQPADT4g/edit?disco=AAAAc3SBnZQ
-                  isCamp && (
-                    <FormSubsection
-                      header="Propagovat akci v Roverském kmeni"
-                      required={isCamp && hasVIPPropagationOption}
-                      help="Placená propagace vaší vícedenní akce v časopisu Roverský kmen za poplatek 1 000 Kč."
-                    >
-                      <FormInputError>
-                        <Controller
-                          name="vip_propagation.rover_propagation"
-                          control={control}
-                          // TODO validation requirements not clear
-                          // TODO this field won't get saved if vip propagation is not filled
-                          rules={{ ...requireBoolean }}
-                          render={({ field }) => (
-                            <InlineSection>
-                              {[
-                                { name: 'ano', value: true },
-                                { name: 'ne', value: false },
-                              ].map(({ name, value }) => (
-                                <div key={name}>
-                                  <label
-                                    htmlFor={name}
-                                    className={'radioLabel'}
-                                  >
-                                    <input
-                                      ref={field.ref}
-                                      type="radio"
-                                      name={field.name}
-                                      id={name}
-                                      value={String(value)}
-                                      checked={field.value === value}
-                                      onChange={e =>
-                                        field.onChange(
-                                          e.target.value === 'true'
-                                            ? true
-                                            : e.target.value === 'false'
-                                            ? false
-                                            : undefined,
-                                        )
-                                      }
-                                    />{' '}
-                                    {name}
-                                  </label>
-                                </div>
-                              ))}
-                            </InlineSection>
-                          )}
-                        />
-                      </FormInputError>
-                    </FormSubsection>
-                  )
-                */}
-                </>
-              )
-          }
         </FormSectionGroup>
       </form>
     </FormProvider>
